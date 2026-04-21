37aa18fdafb287e6e81b18ec22b35a123d059524
Timi16 <timilehinolowu46@gmail.com>
2026-04-20 18:28:00 -0700

    Adding runtime error fix

--- lib/reown-runtime.ts
+++ lib/reown-runtime.ts
@@ -1,14 +1,11 @@
-import * as reownEthersRuntimeModule from '@reown/appkit-ethers-react-native/lib/commonjs/index.js';
-import * as reownRuntimeModule from '@reown/appkit-react-native/lib/commonjs/index.js';
-
 type ReownRuntime = Pick<
   typeof import('@reown/appkit-react-native'),
   'AppKit' | 'AppKitButton' | 'AppKitProvider' | 'createAppKit' | 'useAccount'
 >;
 type ReownEthersRuntime = Pick<typeof import('@reown/appkit-ethers-react-native'), 'EthersAdapter'>;
 
-const reownRuntime = reownRuntimeModule as ReownRuntime;
-const reownEthersRuntime = reownEthersRuntimeModule as ReownEthersRuntime;
+const reownRuntime = require('@reown/appkit-react-native/lib/commonjs/index.js') as ReownRuntime;
+const reownEthersRuntime = require('@reown/appkit-ethers-react-native/lib/commonjs/index.js') as ReownEthersRuntime;
 
 export const { AppKit, AppKitButton, AppKitProvider, createAppKit, useAccount } = reownRuntime;
 export const { EthersAdapter } = reownEthersRuntime;
